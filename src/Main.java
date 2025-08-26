8bad4a069bdefbffabe8f9defabf7187e45c2aa0
alexnezumi <alexsilva.viana2006@gmail.com>
2025-08-26 11:30:47 -0300

    exercicio 2

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+       //exercicio 1 - Crie um programa no qual o usuário deve digitar um número inteiro e o
+        //programa retorna se o número é positivo ou negativo.
+        /*
+        Scanner grava = new Scanner(System.in);
+        int x;
+        System.out.println("Digite um número do tipo inteiro positivo ou negativo");
+        x = grava.nextInt();
+        if(x >= 0)
+        {
+         System.out.println("O numero é positivo");
+        }else
+        {
+        System.out.println("O numero é negativo");
+        }
+        */
+
+      //exercicio 2 -  Crie um programa no qual o usuário deve digitar um número inteiro e o
+        //programa retorna se o número é par ou ímpar.
+       /*
+        Scanner grava = new Scanner(System.in);
+        int x;
+        System.out.println("Digite um numero do tipo inteiro para saber se ele é par ou impar");
+        x = grava.nextInt();
+        if(x % 2 == 0) {
+        System.out.println("Ele é par");
+        }else {
+        System.out.println("Ele é impar");
+        }
+
+        */
+    }
+}
